0f4707cdaaeb3c288554bf1f8a67d8204bfdcbc8
lzm520 <841989963@qq.com>
2021-11-02 13:50:55 +0800

    lzm update

--- extract_patients_from_self_report.py
+++ extract_patients_from_self_report.py
@@ -1,10 +1,12 @@
-'''从self-report中提取出某一种病的病人'''
+""" 从self-report中提取出某一种病的病人 """
 import numpy as np
 import pandas as pd
+
 
 def func():
     file_path = '../data/field_extraction/testing.json'
     df = pd.read_json(file_path)
+
 
 if __name__ == '__main__':
     pass
--- hes_diag_filter-Coronary artery disease.py
+++ hes_diag_filter-Coronary artery disease.py
@@ -4,13 +4,14 @@
 import re
 import json
 import numpy as np
+from collections import defaultdict
 
 
 def HES_diagnosis():
     n_participants = 502506
 
     disease_name = 'Coronary artery disease'
-    hes_diag = list()
+    hes_diag = dict()
 
     hesdiag_file = open('../HES/hesin_diag.txt', 'r')
     ind_eid = np.load('../data/field_extraction/eids.npy')
@@ -43,19 +44,31 @@ def HES_diagnosis():
         '''在此位置设置想要抽取的疾病ICD9和ICD10'''
         if level == '1':
             flag = False
-            diag = {'ukb_index': str(index), 'eid': str(eid), 'ins_index': str(ins_index), 'icd9': '', 'icd10': '',
-                    'disease_name': disease_name}
+            diag = defaultdict()
+            diag = {'ukb_index': str(index), 'eid': str(eid), 'icd9': '', 'icd10': ''}
 
-            if re.match(r'I20*', ICD10) or re.match(r'I21*', ICD10) or re.match(r'I22*', ICD10) or re.match(r'I23*', ICD10) or re.match(r'I241', ICD10) or re.match(r'I252', ICD10):
+            if re.match(r'I20.*', ICD10) or re.match(r'I21.*', ICD10) or re.match(r'I22.*', ICD10) \
+                    or re.match(r'I23.*', ICD10) or re.match(r'I241', ICD10) or re.match(r'I252', ICD10):
                 diag['icd10'] = str(ICD10)
+                diag['Coronary artery disease'] = True
                 flag = True
 
-            if re.match(r'410*', ICD9) or re.match(r'4110*', ICD9) or re.match(r'412*', ICD9) or re.match(r'42979',  ICD9):
+            if re.match(r'410.*', ICD9) or re.match(r'4110.*', ICD9) or re.match(r'412.*', ICD9) \
+                    or re.match(r'42979',  ICD9):
                 diag['icd9'] = str(ICD9)
+                diag['Coronary artery disease'] = True
                 flag = True
 
             if flag:
-                hes_diag.append(diag)
+                if hes_diag.get(diag['eid']):
+                    if diag['icd9']:
+                        if not re.search(diag['icd9'], hes_diag[diag['eid']]['icd9']):
+                            hes_diag[diag['eid']]['icd9'] = hes_diag[diag['eid']]['icd9'] + '&' + diag['icd9']
+                    if diag['icd10']:
+                        if not re.search(diag['icd10'], hes_diag[diag['eid']]['icd10']):
+                            hes_diag[diag['eid']]['icd10'] = hes_diag[diag['eid']]['icd10'] + '&' + diag['icd10']
+                else:
+                    hes_diag[diag['eid']] = diag
 
     hesdiag_file.close()
 
